ba7898fefbfdb2819627985b12329670976116bc
merdan <30455053+maidetective@users.noreply.github.com>
2021-04-29 13:55:55 +0800

    23123123
    --story=1002026 --user=买尔旦 29 https://www.tapd.cn/36072496/s/1000399

---
+++ 文件逐行读取.py
@@ -0,0 +1,9 @@
+file = open("new.txt","r",encoding = "utf-8")
+while True:
+    text = file.readline()
+    if len(text)  != 0:
+        print(text,end = "")
+    else:
+        pass
+        break
+    
